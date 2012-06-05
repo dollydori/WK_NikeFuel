98ceeb1761bd3a81440480c57aa7fb5f766b6d15
Dori Lin <dollydori@gmail.com>
2012-06-05 15:51:45 +0800

    bug fix

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ header('Content-Type: text/html; charset=UTF-8');
 define( "WB_AKEY" , '' );
 define( "WB_SKEY" , '' );
 define( "WB_CALLBACK_URL" , 'http://wkshanghai.com/test/fuel/callback.php' );
+?>
